e8c3c3960d9c7507534bb96631bc7d9561cf0312
Aleuks <aleks@lepd.com>
2024-08-23 18:15:16 +0200

    changements de noms en rapide

---
+++ gui/pullconfig.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CURRENT_DATE=$(date +"%Y-%m-%d")
+COMPUTER_NAME=$(hostname)
+
+# Define the source files
+HYPRCONFFILE="/gui/hypr/hyprland.conf"
+
+# Define the target directory
+HYPRCONF_DIR="~/.config/hypr/"
+
+git pull
+
+# Copy the files to the target directory and overwrite if they already exist
+cp -f "$HYPRCONFFILE" "$HYPRCONF_DIR"
+
+# Print a message indicating that the files have been copied
+echo "### Fin - Tout est bien récupéré depuis le git et implanté sur $COMPUTER_NAME ###"
